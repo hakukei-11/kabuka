5b672acc2b59e4af1534feb01a4768e27c5636ac
hakukei-11 <kitada314@yahoo.co.jp>
2026-07-08 18:30:55 +0900

    Create tickers.py
    new file

---
+++ tickers.py
@@ -0,0 +1,20 @@
+# 日本株ティッカー
+JP_TICKERS = {
+    "7203.T": "トヨタ自動車",
+    "6758.T": "ソニーグループ",
+    "8306.T": "三菱UFJ FG",
+    "7974.T": "任天堂",
+    "9984.T": "ソフトバンクグループ",
+    "6501.T": "日立製作所",
+    "4502.T": "武田薬品工業"
+}
+
+# 米国株ティッカー
+US_TICKERS = {
+    "AAPL": "Apple",
+    "MSFT": "Microsoft",
+    "NVDA": "NVIDIA",
+    "AMZN": "Amazon",
+    "GOOGL": "Alphabet",
+    "META": "Meta Platforms"
+}
